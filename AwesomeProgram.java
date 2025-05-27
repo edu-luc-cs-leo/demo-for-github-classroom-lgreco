58044a68023e0df8380e6f6353ff2ef796c481bb
Leo Irakliotis <lgreco@gmail.com>
2025-05-27 15:40:49 +0000

    tuesday demo

--- AwesomeProgram.java
+++ AwesomeProgram.java
@@ -2,5 +2,6 @@ public class AwesomeProgram {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("this is my first program");
+        System.out.println("I made this change!!!")
     }
 }
